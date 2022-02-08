a103010435457c6daba2a34986073253ff68161c
mmdev110 <mmdev8787@gmail.com>
2022-02-08 20:04:09 +0900

    change folder name

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,17 @@ terraform {
     region = "ap-northeast-1"
   }
 }
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+variable "example_instance_type" {
+  default = "t3.micro"
+}
 resource "aws_instance" "example" {
   ami           = "ami-03d79d440297083e3"
-  instance_type = "t3.micro"
+  instance_type = var.example_instance_type
   tags = {
     Name = "example"
   }
@@ -16,4 +24,9 @@ resource "aws_instance" "example" {
   yum install -y httpd
   systemctl start httpd.service
 EOF
+}
+
+output "example_instance_id" {
+  value = aws_instance.example.id
+
 }
