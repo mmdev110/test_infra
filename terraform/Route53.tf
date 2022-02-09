54cdd1c00430d4d8e1061fc246f577f4fc623251
mmdev110 <mmdev8787@gmail.com>
2022-02-09 20:24:06 +0000

    add route53

---
+++ terraform/Route53.tf
@@ -0,0 +1,20 @@
+//ホストゾーンは既存のものを使用する
+//freenomへのnameserver登録が面倒なため
+data "aws_route53_zone" "example" {
+  name = "mmdev110.tk"
+}
+
+resource "aws_route53_record" "example" {
+  zone_id = data.aws_route53_zone.example.zone_id
+  name    = data.aws_route53_zone.example.name
+  type    = "A"
+  alias {
+    name                        = aws_lb.example.dns_name
+    zone_id                     = aws_lb.example.zone_id
+    evaluevaluate_target_health = true
+  }
+}
+
+output "domain_name" {
+  value = aws_route53_record.example.name
+}
