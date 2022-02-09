e081fe2aa8943ee55af5a2123b0cca82ffca0364
mmdev110 <mmdev8787@gmail.com>
2022-02-09 20:26:00 +0000

    fix

--- terraform/Route53.tf
+++ terraform/Route53.tf
@@ -9,9 +9,9 @@ resource "aws_route53_record" "example" {
   name    = data.aws_route53_zone.example.name
   type    = "A"
   alias {
-    name                        = aws_lb.example.dns_name
-    zone_id                     = aws_lb.example.zone_id
-    evaluevaluate_target_health = true
+    name                   = aws_lb.example.dns_name
+    zone_id                = aws_lb.example.zone_id
+    evaluate_target_health = true
   }
 }
 
