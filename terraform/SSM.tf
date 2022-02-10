1900c70e7fcfd5a34d17a9d05611b610804aaff9
mmdev110 <mmdev8787@gmail.com>
2022-02-10 10:35:58 +0000

    db redis

--- terraform/SSM.tf
+++ terraform/SSM.tf
@@ -26,3 +26,9 @@ resource "aws_ssm_parameter" "db_password" {
     ignore_changes = [value]
   }
 }
+resource "aws_ssm_parameter" "db_raw_password" {
+  name        = "/db/raw_password"
+  value       = "VeryStrongPassword!"
+  type        = "SecureString"
+  description = "データベースのパスワード"
+}
