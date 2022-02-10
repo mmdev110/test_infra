d8d4db49a94c05cd953809828698a2e9f54e4b93
mmdev110 <mmdev8787@gmail.com>
2022-02-10 08:33:19 +0000

    ecs batch

---
+++ terraform/Batch.tf
@@ -0,0 +1,50 @@
+//バッチ関連
+resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
+  name              = "/ecs-scheduled-tasks/example"
+  retention_in_days = 180
+}
+
+resource "aws_ecs_task_definition" "example_batch" {
+  family                   = "example-batch"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = file("./batch_container_definitions.json")
+  execution_role           = module.ecs_task_execution_role.iam_role_arn
+}
+
+//cloudwatchからECSを実行するためのロール
+module "ecs_events_role" {
+  source     = "./iam_role"
+  name       = "ecs-events"
+  identifier = "events.amazonaws.com"
+  policy     = data.aws_iam_policy.ecs_events_role_policy.policy
+}
+//用意されてるポリシーを使用する
+data "aws_iam_policy" "ecs_events_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
+}
+
+//ジョブの実行スケジュール
+resource "aws_cloudwatch_event_rule" "example_batch" {
+  name                = "example-batch"
+  description         = "とても重要なバッチ"
+  schedule_expression = "cron(*/2 * * * ? *)"
+}
+//どのジョブを実行するのか
+resource "aws_cloudwatch_event_target" "example_batch" {
+  target_id = "example_batch"
+  rule      = aws_cloudwatch_event_rule.example_batch.name
+  arn       = aws_ecs_cluster.example.arn //対象のクラスター
+
+  ecs_target {
+    launch_type         = "FARGATE"
+    task_count          = 1
+    task_definition_arn = aws_ecs_task_definition.example_batch.arn
+    network_configuration {
+      assign_public_ip = "false"
+      subnets          = [aws_subet.private_0.id]
+    }
+  }
+}
