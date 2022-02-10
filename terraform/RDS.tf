bcfb82cbcd06be5f5c89a7f03019faba62900983
mmdev110 <mmdev8787@gmail.com>
2022-02-10 11:15:14 +0000

    fix

--- terraform/RDS.tf
+++ terraform/RDS.tf
@@ -55,7 +55,7 @@ resource "aws_db_instance" "example" {
   削除するときは逆にする
   */
   deletion_protection    = false
-  skip_final_snapshot    = false
+  skip_final_snapshot    = true
   port                   = 3306
   apply_immediately      = false
   vpc_security_group_ids = [module.mysql_sg.security_group_id]
